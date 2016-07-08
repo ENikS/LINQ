be3f5d7614d33f429a863bc9fde6dcbd0b27a794
Eugene Sadovoi <evgeni@eniks.com>
2016-07-08 16:49:03 -0400

    Update example.js

--- dist/example.js
+++ dist/example.js
@@ -1,5 +1,4 @@
-// This example requires latest node version 6.2
-
+// NOTE: This example requires node version 6 or later
 var Enumerable = require("linq-es2015");
  
 var count =  Enumerable.asEnumerable( [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] )
