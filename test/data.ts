73722e87bb7a613d8141b93f8e07687ccfc834c7
ENikS <evgeni@eniks.com>
2017-03-10 19:12:14 -0500

    Expanding code coverage

--- test/data.ts
+++ test/data.ts
@@ -42,6 +42,7 @@ export var fruits = [
 ];
 
 export var simpleArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+export var randomArray = [ 5, 8, 9, 6, 7, 10, 1, 2, 3, 4  ];
 export var oddArray = [ 1, 3, 5, 7, 9, 11, 13, 15 ];
 
 
