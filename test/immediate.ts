03696119fd188a663f5c5743e9764324debc5fba
gerneio <hector.bas.iii@basdeals.com>
2019-01-11 18:51:01 -0600

    Update immediate.ts
    Remove Number test case. Only applicable to browserfied code.

--- test/immediate.ts
+++ test/immediate.ts
@@ -37,11 +37,6 @@ describe('Immediate Execution -', function () {
     it('Aggregate() - Default Value [String]', function () {
         assert.equal("123", Linq.From([1, 2, 3]).Aggregate("", (c, n) => c.toString() + n.toString()));
     });
-    
-    it('Aggregate() - Default Value [Number]', function () {
-        assert.equal("0123", Linq.From(["1", "2", "3"]).Aggregate(0, (c, n) => c.toString() + n.toString()));
-    });
-
 
 
     // All
